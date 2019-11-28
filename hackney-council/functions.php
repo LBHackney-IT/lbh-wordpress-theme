17fe784d9e3996d15616e261c55a9b7795ef5f54
Emma Lewis <emma.lewis@hackney.gov.uk>
2019-11-28 16:46:30 +0000

    WIP

--- hackney-council/functions.php
+++ hackney-council/functions.php
@@ -98,6 +98,9 @@ function html5blank_header_scripts()
         wp_register_script('modernizr', get_template_directory_uri() . '/js/lib/modernizr-2.7.1.min.js', array(), '2.7.1'); // Modernizr
         wp_enqueue_script('modernizr'); // Enqueue it!
 
+        wp_register_script('hackney-wordpress', get_template_directory_uri() . '/dist/hackney-wordpress.min.js', '1.0.0'); // Custom scripts
+        wp_enqueue_script('hackney-wordpress'); // Enqueue it!
+
         wp_register_script('html5blankscripts', get_template_directory_uri() . '/js/scripts.js', array('jquery'), '1.0.0'); // Custom scripts
         wp_enqueue_script('html5blankscripts'); // Enqueue it!
     }
@@ -117,6 +120,9 @@ function html5blank_styles()
 {
     wp_register_style('normalize', get_template_directory_uri() . '/normalize.css', array(), '1.0', 'all');
     wp_enqueue_style('normalize'); // Enqueue it!
+
+    wp_register_style('hackney-wordpress', get_template_directory_uri() . '/dist/all.css', array(), '1.0', 'all');
+    wp_enqueue_style('hackney-wordpress'); // Enqueue it!
 
     wp_register_style('html5blank', get_template_directory_uri() . '/style.css', array(), '1.1', 'all');
     wp_enqueue_style('html5blank'); // Enqueue it!
