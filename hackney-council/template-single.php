9f430d777fcaf8f5fc7ac65f7a6bade5d0bef6dc
Emma Lewis <emma.lewis@hackney.gov.uk>
2019-12-09 11:32:41 +0000

    Fix page containers

--- hackney-council/template-single.php
+++ hackney-council/template-single.php
@@ -17,68 +17,76 @@
               <p class="lbh-category"><?php _e( 'Categorised in: ', 'html5blank' ); the_category(', ');  ?></p>        
               <span class="lbh-comments"><?php if (comments_open( get_the_ID() ) ) comments_popup_link( __( 'Leave your thoughts', 'html5blank' ), __( '1 Comment', 'html5blank' ), __( '% Comments', 'html5blank' )); ?></span>
             <?php endif ?>
-          </div>
-        <?php endif; ?>
-        <?php // $accordion_counter helps us to make sure that accordion ids are unique ?>
-        <?php $accordion_counter = 0; ?>
-        <?php if( have_rows('lbh_page_builder') ): ?>
-          <?php while ( have_rows('lbh_page_builder') ) : the_row(); ?>
-            <?php if( get_row_layout() == 'content' ): ?>
-              <div class="lbh-container">
+          <?php endif; ?>
+          <?php // $accordion_counter helps us to make sure that accordion ids are unique ?>
+          <?php $accordion_counter = 0; ?>
+          <?php if( have_rows('lbh_page_builder') ): ?>
+            <?php while ( have_rows('lbh_page_builder') ) : the_row(); ?>
+              <?php if( get_row_layout() == 'content' ): ?>
                 <?php get_template_part('component-content'); ?>
-              </div>
-            <?php elseif( get_row_layout() == 'featured_message' ): ?>
-              <div class="lbh-container">
+              <?php elseif( get_row_layout() == 'featured_message' ): ?>
                 <?php get_template_part('component-page-announcement'); ?>
-              </div>
-            <?php elseif( get_row_layout() == 'accordion' ): ?>
-              <?php // $accordion_counter helps us to make sure that accordion ids are unique ?>
-              <?php $accordion_counter++; ?>
-              <div class="lbh-container">
+              <?php elseif( get_row_layout() == 'accordion' ): ?>
+                <?php // $accordion_counter helps us to make sure that accordion ids are unique ?>
+                <?php $accordion_counter++; ?>
                 <?php include( locate_template( 'component-accordion.php', false, false ) ); ?>
-              </div>
-            <?php elseif( get_row_layout() == 'anchor_links' ): ?>
-              <div class="lbh-container">
+              <?php elseif( get_row_layout() == 'anchor_links' ): ?>
                 <?php get_template_part('component-anchor-links'); ?>
-              </div>
-            <?php elseif( get_row_layout() == 'iframes' ): ?>
-              <div class="lbh-container">
+              <?php elseif( get_row_layout() == 'iframes' ): ?>
                 <?php get_template_part('component-iframe'); ?>
-              </div>
-            <?php elseif( get_row_layout() == 'image_block_single' ): ?>
-              <div class="lbh-container">
+              <?php elseif( get_row_layout() == 'image_block_single' ): ?>
                 <?php get_template_part('component-image-landscape'); ?>
-              </div>
-            <?php elseif( get_row_layout() == 'image_block_single_portrait' ): ?>
-              <div class="lbh-container">
+              <?php elseif( get_row_layout() == 'image_block_single_portrait' ): ?>
                 <?php get_template_part('component-image-portrait'); ?>
-              </div>
-            <?php elseif( get_row_layout() == 'image_block_double' ): ?>
-              <div class="lbh-container">
+              <?php elseif( get_row_layout() == 'image_block_double' ): ?>
                 <?php get_template_part('component-image-double'); ?>
-              </div>
-            <?php elseif( get_row_layout() == 'image_block_triple' ): ?>
-              <div class="lbh-container">
+              <?php elseif( get_row_layout() == 'image_block_triple' ): ?>
                 <?php get_template_part('component-image-triple'); ?>
-              </div>
-            <?php elseif( get_row_layout() == 'article_listings' ): ?>
-              <?php get_template_part('component-article-listings'); ?>
-            <?php elseif( get_row_layout() == 'promo' ): ?>
-              <?php get_template_part('component-promo'); ?>
-            <?php elseif( get_row_layout() == 'news' ): ?>
-              <?php get_template_part('component-news-listings'); ?>
-            <?php elseif( get_row_layout() == 'blocks' ): ?>
-              <?php get_template_part('component-blocks'); ?>
+              <?php elseif( get_row_layout() == 'article_listings' ): ?>
+                <?php if(!is_front_page()) : ?>
+                  </div>
+                <?php endif; ?>
+                <?php get_template_part('component-article-listings'); ?>
+                <?php if(!is_front_page()) : ?>
+                  <div class="lbh-container">
+                <?php endif; ?>
+              <?php elseif( get_row_layout() == 'promo' ): ?>
+                <?php if(!is_front_page()) : ?>
+                  </div>
+                <?php endif; ?>
+                  <?php get_template_part('component-promo'); ?>
+                <?php if(!is_front_page()) : ?>
+                  <div class="lbh-container">
+                <?php endif; ?>
+              <?php elseif( get_row_layout() == 'news' ): ?>
+                <?php if(!is_front_page()) : ?>
+                  </div>
+                <?php endif; ?>
+                <?php get_template_part('component-news-listings'); ?>
+                <?php if(!is_front_page()) : ?>
+                  <div class="lbh-container">
+                <?php endif; ?>
+              <?php elseif( get_row_layout() == 'blocks' ): ?>
+                <?php if(!is_front_page()) : ?>
+                  </div>
+                <?php endif; ?>
+                  <?php get_template_part('component-blocks'); ?>
+                <?php if(!is_front_page()) : ?>
+                  <div class="lbh-container">
+                <?php endif; ?>
+              <?php endif; ?>
+            <?php endwhile; ?>
+          <?php endif; ?>
+          <?php if(!is_front_page()) : ?>
+            <?php if (get_post_type == 'post') : ?>
+              <?php comments_template( '', true ); ?>
             <?php endif; ?>
-          <?php endwhile; ?>
-        <?php endif; ?>
+            <?php edit_post_link(); ?>
+          <?php endif; ?>
+        </div>
         <?php if(is_front_page()) : ?>
           <?php get_template_part('component-social-blocks'); ?>
         <?php endif; ?>
-        <?php if (get_post_type == 'post') : ?>
-          <?php comments_template( '', true ); ?>
-        <?php endif; ?>
-        <?php edit_post_link(); ?>
       </article>
     <?php endwhile; ?>
   <?php else: ?>
