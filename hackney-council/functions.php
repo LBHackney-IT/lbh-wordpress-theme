174ec1fd7ea34d4d392aaa36e541b9a440a2a66a
Emma Lewis <emma.lewis@hackney.gov.uk>
2019-12-19 12:13:43 +0000

    iterate js version

--- hackney-council/functions.php
+++ hackney-council/functions.php
@@ -107,7 +107,7 @@ function html5blank_header_scripts()
         wp_register_script('modernizr', get_template_directory_uri() . '/js/lib/modernizr-2.7.1.min.js', array(), '2.7.1'); // Modernizr
         wp_enqueue_script('modernizr'); // Enqueue it!
 
-        wp_register_script('hackney-wordpress', get_template_directory_uri() . '/dist/hackney-wordpress.min.js', array(), '1.0.2', true); // Custom scripts
+        wp_register_script('hackney-wordpress', get_template_directory_uri() . '/dist/hackney-wordpress.min.js', array(), '1.0.3', true); // Custom scripts
         wp_enqueue_script('hackney-wordpress'); // Enqueue it!
     }
 }
