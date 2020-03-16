ce592367b009ff3169a11ea36a5c402b97af840a
Emma Lewis <emmarachellewis@gmail.com>
2020-03-16 16:05:36 +0000

    Update hero to allow title and intro link

--- hackney-council/component-hero.php
+++ hackney-council/component-hero.php
@@ -2,23 +2,29 @@
   <div class="lbh-hero__wrapper" style="background-image:url(<?php echo $hero['hero_banner']; ?>)">
     <div class="lbh-hero__content">
       <div class="lbh-container">
-        <div class="lbh-hero__copy">
+        <?php if ($hero['hero_title_and_intro_link'] !== '') : ?>
+          <a href="<?php echo $hero['hero_title_and_intro_link']; ?>" class="lbh-hero__copy lbh-hero__copy--link">
+        <?php else : ?>
+          <div class="lbh-hero__copy">
+        <?php endif; ?>
           <h2 class="lbh-heading-h2"><?php echo $hero['hero_title']; ?></h2>
           <p class="lbh-body-m"><?php echo $hero['hero_intro_text']; ?></p>
-        </div>
+        <?php if ($hero['hero_title_and_intro_link']) : ?>
+          </a>
+        <?php else : ?>
+          </div>
+        <?php endif; ?>
       </div>
     </div>
-    <div class="lbh-container">
-      <div class="lbh-hero__box">
-        <h3 class="lbh-heading-h4 lbh-hero__box-title"><?php echo $hero['hero_box_title']; ?></h3>
-        <ul class="lbh-list lbh-list--bullet lbh-hero__box-list">
-          <?php if(count($hero['hero_box_items']) > 0): ?>
-            <?php foreach ( $hero['hero_box_items'] as $item ) : the_row(); ?>
-              <li><a href="<?php echo $item['cta_url']; ?>" class="lbh-hero__box-link lbh-link"><?php echo $item['cta_url_text']; ?></a></li>
-            <?php endforeach; ?>
-          <?php endif; ?>
-        </ul>
-      </div>
+    <div class="lbh-hero__box">
+      <h3 class="lbh-heading-h4 lbh-hero__box-title"><?php echo $hero['hero_box_title']; ?></h3>
+      <ul class="lbh-list lbh-list--bullet lbh-hero__box-list">
+        <?php if(count($hero['hero_box_items']) > 0): ?>
+          <?php foreach ( $hero['hero_box_items'] as $item ) : the_row(); ?>
+            <li><a href="<?php echo $item['cta_url']; ?>" class="lbh-hero__box-link lbh-link"><?php echo $item['cta_url_text']; ?></a></li>
+          <?php endforeach; ?>
+        <?php endif; ?>
+      </ul>
     </div>
   </div>
   <div class="lbh-hero__cta-bar">
