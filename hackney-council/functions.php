f9db339c9aa33b24be18ae71c8c914da52002c1c
Jayson Hunter <github@jaysonhunter.com>
2020-05-05 12:52:58 +0100

    Hooked the acf_add_optjons_page call into the 'plugins_loaded' action.

--- hackney-council/functions.php
+++ hackney-council/functions.php
@@ -426,7 +426,14 @@ function my_acf_block_render_callback( $block ) {
 add_filter( 'acf/rest_api/field_settings/show_in_rest', '__return_true' );
 
 // add options page
-acf_add_options_page();
+// Add Options Page
+function add_my_options_page() {
+    if( function_exists('acf_add_options_page') ) {
+      acf_add_options_page();
+    }
+ }
+
+add_action( 'plugins_loaded', 'add_my_options_page' );
 
 
 /**
